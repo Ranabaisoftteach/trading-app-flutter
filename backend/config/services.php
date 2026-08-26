2eed4298667b34aa4f450943c3817a8b8c3f2c57
Ranabaisoftteach <krishnaitsolution0@gmail.com>
2026-08-26 17:42:05 +0530

    Add Angel One SmartAPI configuration

--- backend/config/services.php
+++ backend/config/services.php
@@ -6,4 +6,11 @@ return [
         'app_key' => env('HYPERVERGE_APP_KEY'),
         'vendor_validation_url' => env('HYPERVERGE_VENDOR_VALIDATION_URL', 'https://ind-engine.thomas.hyperverge.co/v1/async/vendorValidation'),
     ],
+    'angelone' => [
+        'base_url' => env('ANGELONE_BASE_URL', 'https://apiconnect.angelone.in'),
+        'api_key' => env('ANGELONE_API_KEY'),
+        'client_local_ip' => env('ANGELONE_CLIENT_LOCAL_IP', '127.0.0.1'),
+        'client_public_ip' => env('ANGELONE_CLIENT_PUBLIC_IP'),
+        'mac_address' => env('ANGELONE_MAC_ADDRESS', '00:00:00:00:00:00'),
+    ],
 ];
